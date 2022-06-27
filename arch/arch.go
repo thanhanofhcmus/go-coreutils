acdfe187904d5ad5556f1085b82a3989965fe101
Nguyễn Thanh An <55699606+thanhanofhcmus@users.noreply.github.com>
2022-06-27 23:04:21 +0700

    add arch

--- arch/arch.go
+++ arch/arch.go
@@ -1,11 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"runtime"
+	"os"
+
+	"golang.org/x/sys/unix"
 )
 
+func clen(b []byte) int {
+	for i, v := range b {
+		if v == 0 {
+			return i
+		}
+	}
+	return len(b)
+}
+
+func cString(b []byte) string {
+	return string(b[:clen(b)])
+}
+
 func main() {
-	fmt.Println("arch: ", runtime.GOARCH)
-	fmt.Println("os: ", runtime.GOOS)
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "arch - print machine hardware name (same as uname -m)\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if args := flag.Args(); len(args) != 0 {
+		fmt.Println("too many arguments")
+		os.Exit(1)
+	}
+
+	utsname := &unix.Utsname{}
+	err := unix.Uname(utsname)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	fmt.Println(cString(utsname.Machine[:]))
+
 }
