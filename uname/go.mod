dbf06636b63b37fcec6fa7db9fdd8fff5afa867c
Nguyễn Thanh An <55699606+thanhanofhcmus@users.noreply.github.com>
2022-06-27 22:59:53 +0700

    add uname

---
+++ uname/go.mod
@@ -0,0 +1,5 @@
+module an/go-coreutils-uname
+
+go 1.18
+
+require golang.org/x/sys v0.0.0-20220624220833-87e55d714810 // indirect
---
+++ uname/uname.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/sys/unix"
+)
+
+func clen(b []byte) int {
+	for i, v := range b {
+		if v == 0 {
+			return i
+		}
+	}
+	return len(b)
+}
+
+func cString(b []byte) string {
+	return string(b[:clen(b)])
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "uname - print system infomation\n")
+		flag.PrintDefaults()
+	}
+
+	argOsName := flag.Bool("s", false, "Print the operating system name")
+	argAll := flag.Bool("a", false, "Print all infomation (as if running with mnrsv)")
+	argOsRelease := flag.Bool("r", false, "Print the operating system release")
+	argOsVersion := flag.Bool("v", false, "Print the operating system version")
+	argNodeName := flag.Bool("n", false, "Print the network nodename")
+	argMachineName := flag.Bool("m", false, "Print the machine name")
+
+	flag.Parse()
+
+	if args := flag.Args(); len(args) != 0 {
+		fmt.Println("too many arguments")
+		os.Exit(1)
+	}
+
+	utsname := &unix.Utsname{}
+	err := unix.Uname(utsname)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	osName := cString(utsname.Sysname[:])
+	osRelease := cString(utsname.Release[:])
+	osVersion := cString(utsname.Version[:])
+	nodeName := cString(utsname.Nodename[:])
+	machineName := cString(utsname.Machine[:])
+
+	if *argAll {
+		fmt.Printf("%s %s %s %s %s\n", machineName, nodeName, osRelease, osName, osVersion)
+		os.Exit(0)
+	}
+
+	var sb strings.Builder
+
+	if *argOsName {
+		sb.WriteString(osName)
+		sb.WriteRune(' ')
+	}
+	if *argNodeName {
+		sb.WriteString(nodeName)
+		sb.WriteRune(' ')
+	}
+	if *argOsRelease {
+		sb.WriteString(osRelease)
+		sb.WriteRune(' ')
+	}
+	if *argOsVersion {
+		sb.WriteString(osVersion)
+		sb.WriteRune(' ')
+	}
+	if *argMachineName {
+		sb.WriteString(machineName)
+		sb.WriteRune(' ')
+	}
+
+	line := sb.String()
+
+	if len(line) == 0 {
+		line = osName
+	} else {
+		line = line[:len(line) - 1] // remove trailing whitespace
+	}
+
+	fmt.Println(line)
+}
