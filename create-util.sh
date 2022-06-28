a659f4df1e992befeb7557a8c9c045900cb6d019
Nguyễn Thanh An <55699606+thanhanofhcmus@users.noreply.github.com>
2022-06-28 15:12:57 +0700

    update create-utils

--- create-util.sh
+++ create-util.sh
@@ -5,6 +5,24 @@ echo $1
 mkdir $1
 cd $1
 go mod init "an/go-coreutils-$1"
-touch "$1.go"
+echo "package main
+
+import (
+	\"flag\"
+	\"fmt\"
+)
+
+const (
+	USAGE = \`$1 - \`
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), USAGE)
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+}
+" >> "$1.go"
 cd ..
 go work use "./$1"
